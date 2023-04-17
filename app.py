22d0730b3ada19d280b5118881e710cd525fbea7
Sandra Grace Nelson <100516887+SandraGraceNelson@users.noreply.github.com>
2023-04-17 12:37:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,13 +5,13 @@ def analyze_text(text):
     analyzer = SentimentIntensityAnalyzer()
     sentiment_scores = analyzer.polarity_scores(text)
     if sentiment_scores['neg'] > sentiment_scores['pos']:
-        return "The text contains language associated with depression."
+        return "Hey, they need some help asap. Be the support."
     else:
-        return "The text does not contain language associated with depression."
+        return "Don't worry, they are perfectly alright."
 
 def run_app():
     # Set page width and page title
-    st.set_page_config(page_title="Depression Analysis App", page_icon=":smiley:", layout="wide")
+    st.set_page_config(page_title="DepScan", page_icon=":smiley:", layout="wide")
     
     # Set background color
     page_bg = '''
@@ -27,11 +27,11 @@ def run_app():
     st.markdown(page_bg, unsafe_allow_html=True)
 
     # Set app title and subtitle
-    st.title("Depression Analysis App")
-    st.markdown("*Analyze text to determine if it contains language associated with depression.*")
-
+    st.title("DepScan")
+    st.markdown("*Do you find it difficult to understand the emotions of your loved ones? Don't worry, DepScan can help! Just input their text and DepScan will analyze it to determine if it contains language commonly associated with depression.*")
+    st.markdown("*Time is of the essence when it comes to mental health, so don't hesitate to use DepScan.*")
     # Create text input and analyze button
-    text = st.text_area("Enter your text:", height=150)
+    text = st.text_area("Enter text:", height=150)
     if st.button("Analyze"):
         result = analyze_text(text)
         st.write(result)
